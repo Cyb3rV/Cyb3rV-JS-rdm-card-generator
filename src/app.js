b5f998295b2a9916b0fee2a3b4c6e57082e28ed9
Cyb3rV <152754161+Cyb3rV@users.noreply.github.com>
2024-02-23 21:18:38 +0000

    Finished

--- src/app.js
+++ src/app.js
@@ -2,10 +2,69 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function getRandomNumber(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function checkColor(suit) {
+  if (suit === "♥" || suit === "♦") return "red";
+  else return "black";
+}
+
+const suits = ["♦", "♥", "♠", "♣"];
+const values = [
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "K",
+  "Q",
+  "J",
+  "A"
+];
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  const currentSuit = getRandomNumber(4);
+  const currentValue = getRandomNumber(13);
+
+  console.log(`${suits[currentSuit]} ${values[currentValue]}`);
+  document.body.style.background = "green";
+
+  const tlSuit = document.createElement("p");
+  const value = document.createElement("p");
+  const brSuit = document.createElement("p");
+
+  tlSuit.className = "col-auto";
+  tlSuit.textContent = suits[currentSuit];
+  tlSuit.style.fontSize = "120px";
+  tlSuit.style.textAlign = "left";
+  tlSuit.style.color = checkColor(suits[currentSuit]);
+
+  value.className = "col-auto";
+  value.textContent = values[currentValue];
+  value.style.fontSize = "160px";
+  value.style.textAlign = "center";
+  value.style.fontWeight = "bold";
+
+  brSuit.className = "col-auto";
+  brSuit.textContent = suits[currentSuit];
+  brSuit.style.fontSize = "120px";
+  brSuit.style.textAlign = "right";
+  brSuit.style.color = checkColor(suits[currentSuit]);
+
+  const myDiv = document.querySelector("div");
+
+  myDiv.className = "card mt-3 px-4";
+  myDiv.style.width = "30rem";
+
+  myDiv.appendChild(tlSuit);
+  myDiv.appendChild(value);
+  myDiv.appendChild(brSuit);
 };
